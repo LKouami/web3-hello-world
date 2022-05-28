e30d70168a1da5751e7fd0ed560277fac64b7819
Lucas Kouami <rkouami@outlook.com>
2022-05-28 05:31:14 +0000

    first commit

---
+++ test.js
@@ -0,0 +1,27 @@
+[
+  {
+    inputs: [{ internalType: "string", name: "initMessage", type: "string" }],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    anonymous: false,
+    inputs: [{"indexed":false,"in"type":"string"}],"name":"UpdatedMessages","type":"event"},{"inputs":[],"name":"message","outputs":[{"internalTternalType":"string","name":"oldStr","type":"string"},{"indexed":false,"internalType":"stnputs":[{"internalType":"string","name":"newMessage","type":"string"}],"name":"update","outputs":[],"stateMutabring","name":"newStr","type":"string"}],
+    name: "UpdatedMessages",
+    type: "event",
+  },
+  {
+    inputs: [],
+    name: "message",
+    outputs: [[Object]],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [[Object]],
+    name: "update",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+];
